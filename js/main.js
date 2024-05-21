1cca261752a3aa3a03c6cf51c7564ada9ecc1824
kungrankang@gmail.com <kungrankang@gmail.com>
2024-05-21 18:30:41 +0900

    희승님 비주얼 오류 반응형 글자넘침 수정

--- js/main.js
+++ js/main.js
@@ -18,20 +18,81 @@ window.addEventListener("load", function () {
   });
 
   // ===== chat 버튼 클릭
+  // const chatBtn = document.getElementById('chatBtn');
+  // const chatTextBox = document.querySelector('.chat-text-box');
+
+  // chatBtn.addEventListener('click', function (event) {
+  //     // chat-text-box의 display 속성을 토글하여 나타내거나 숨김
+  //     if (chatTextBox.style.display === 'none' || chatTextBox.style.display === '') {
+  //         chatTextBox.style.display = 'block';
+  //     } else {
+  //         chatTextBox.style.display = 'none';
+  //     }
+  // });
+
+  // ======= chat 버튼 모바일
   const chatBtn = document.getElementById("chatBtn");
   const chatTextBox = document.querySelector(".chat-text-box");
+  const chatTextMobile = document.querySelector(".chat-text-mobile");
+  const chatTextClose = document.querySelector(".chat-text-close > a > img");
 
-  chatBtn.addEventListener("click", function (event) {
-    // chat-text-box의 display 속성을 토글하여 나타내거나 숨김
-    if (
-      chatTextBox.style.display === "none" ||
-      chatTextBox.style.display === ""
-    ) {
-      chatTextBox.style.display = "block";
-    } else {
+  function handleResize() {
+    if (window.innerWidth <= 500) {
       chatTextBox.style.display = "none";
+      chatTextMobile.style.display = localStorage.getItem("chatClosed") === "true" ? "none" : "block";
+    } else {
+      chatTextBox.style.display = localStorage.getItem("chatClosed") === "true" ? "none" : "block";
+      chatTextMobile.style.display = "none";
+    }
+  }
+
+  // 초기 로드 시 한 번 실행하여 상태를 설정
+  document.addEventListener("DOMContentLoaded", function () {
+    handleResize();
+    if (localStorage.getItem("chatClosed") === "true") {
+      chatTextBox.style.display = "none";
+      chatTextMobile.style.display = "none";
     }
   });
+
+  // 윈도우 리사이즈 이벤트 리스너 추가
+  window.addEventListener("resize", handleResize);
+
+  chatBtn.addEventListener("click", function () {
+    if (window.innerWidth <= 500) {
+      // chatTextMobile의 display 속성을 토글하여 나타내거나 숨김
+      if (chatTextMobile.style.display === "none" || chatTextMobile.style.display === "") {
+        chatTextMobile.style.display = "block";
+        localStorage.setItem("chatClosed", "false");
+      } else {
+        chatTextMobile.style.display = "none";
+        localStorage.setItem("chatClosed", "true");
+      }
+    } else {
+      // chatTextBox의 display 속성을 토글하여 나타내거나 숨김
+      if (chatTextBox.style.display === "none" || chatTextBox.style.display === "") {
+        chatTextBox.style.display = "block";
+        localStorage.setItem("chatClosed", "false");
+      } else {
+        chatTextBox.style.display = "none";
+        localStorage.setItem("chatClosed", "true");
+      }
+    }
+  });
+
+  // 닫기 버튼 클릭 이벤트
+  chatTextClose.addEventListener("click", function () {
+    chatTextBox.style.display = "none";
+    chatTextMobile.style.display = "none";
+    localStorage.setItem("chatClosed", "true"); // 로컬 스토리지에 상태 저장
+  });
+  // // ====== chat 버튼 모바일 닫기
+  // const chatTextClose = document.querySelector('.chat-text-close > a > img');
+  // chatTextClose.addEventListener('click', function () {
+  //     chatTextMobile.style.display = 'none';
+  // });
+
+  // })
 
   // //===== 햄버거 메뉴
   // let ham = document.querySelector(".ham-menu");
@@ -70,15 +131,24 @@ window.addEventListener("load", function () {
     var header = document.querySelector(".header");
     var inner = document.querySelector(".inner");
     var headerHeight = header.offsetHeight;
+    // var depth1 = document.querySelector('.depth1');
 
     if (window.pageYOffset >= headerHeight) {
       nav.style.position = "fixed";
-      nav.style.top = "0";
+      nav.style.top = "0px";
+      // nav.style.width = '100%'; // 너비를 100%로 설정
       inner.classList.add("scrolled"); // 클래스 추가
+      // depth1.style.forEach(function (item) {
+      //     item.style.paddingTop = '20px'; // padding-top 설정
+      // });
     } else {
       nav.style.position = "relative";
-      nav.style.top = "0";
+      nav.style.top = "0px";
+      // nav.style.width = ''; // 초기화하여 부모 요소에 따라 너비가 결정되도록 함
       inner.classList.remove("scrolled"); // 클래스 제거
+      // depth1.style.forEach(function (item) {
+      //     item.style.paddingTop = '15px'; // padding-top 설정
+      // });
     }
     inner.classList.add("inner"); // .inner 클래스 추가
   });
@@ -155,10 +225,7 @@ window.addEventListener("load", function () {
     // 현재 날짜와 시간을 기록
     const currentDate = new Date().toLocaleString();
     // sessionStorage에 'hideModal'을 현재 날짜와 시간으로 저장
-    sessionStorage.setItem(
-      "hideModal",
-      JSON.stringify({ value: true, datetime: currentDate })
-    );
+    sessionStorage.setItem("hideModal", JSON.stringify({ value: true, datetime: currentDate }));
 
     // 모달창을 숨김
     allClose.style.display = "none";
@@ -170,13 +237,9 @@ window.addEventListener("load", function () {
 
     if (storedData && storedData.value) {
       // 현재 시간과 저장된 시간 차이를 계산 (밀리초 단위)
-      const timeDifference =
-        new Date().getTime() - new Date(storedData.datetime).getTime();
+      const timeDifference = new Date().getTime() - new Date(storedData.datetime).getTime();
       // 24시간(밀리초 기준)가 지났거나, 새로고침을 했을 경우
-      if (
-        timeDifference >= 24 * 60 * 60 * 1000 ||
-        (window.performance && window.performance.navigation.type === 1)
-      ) {
+      if (timeDifference >= 24 * 60 * 60 * 1000 || (window.performance && window.performance.navigation.type === 1)) {
         sessionStorage.removeItem("hideModal");
       }
     }
